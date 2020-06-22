c4301889a526f2359593f24c8312b7e32a8d60b6
Daniel Gibbs <me@danielgibbs.co.uk>
2020-06-23 00:03:20 +0100

    shellcheck

--- nxfilter-install.sh
+++ nxfilter-install.sh
@@ -1,9 +1,8 @@
-
 #!/bin/bash
 apt-get update
 apt-get upgrade
 apt-get install -y wget curl default-jre
-nxfilterurl=$(curl -s -L https://nxfilter.org/p3/download | grep ".deb" | grep -Eo "(http|https)://[a-zA-Z0-9./?=_-]*" | grep filter-.*deb | grep -v mediafire)
+nxfilterurl=$(curl -s -L https://nxfilter.org/p3/download | grep ".deb" | grep -Eo "(http|https)://[a-zA-Z0-9./?=_-]*" | grep "filter-.*deb" | grep -v "mediafire")
 nxfilterdeb=$(echo ${nxfilterurl} | cut -d'/' -f4-)
 wget "${nxfilterurl}"
 dpkg -i "${nxfilterdeb}"
